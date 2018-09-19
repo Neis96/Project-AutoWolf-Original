7a23bdeea4a40101a07f07728d6b8739e346e9f5
Nicolas Bettega <nicolas96bettega@gmail.com>
2018-09-19 14:16:55 -0300

    Barra de herramienta
    Se integro la barra de herramienta para comodidad del usuario para así poder desarrollar sus tareas con comodidad valga la redundancia

--- ProyectoAutoloboOriginal/ProyectoAutoloboOriginal/Home.Designer.vb
+++ ProyectoAutoloboOriginal/ProyectoAutoloboOriginal/Home.Designer.vb
@@ -22,18 +22,151 @@ Partial Class Home
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NuevoUsuarioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NueToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NuevaReservaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GestionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LiquidacionChoferesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LiquidacionCCorrienteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClientesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AyudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GuiaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContactosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConfiguracionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ResumenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem, Me.VerToolStripMenuItem, Me.GestionToolStripMenuItem, Me.ClientesToolStripMenuItem, Me.AyudaToolStripMenuItem, Me.ConfiguracionToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1005, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoUsuarioToolStripMenuItem, Me.NueToolStripMenuItem, Me.NuevaReservaToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'NuevoUsuarioToolStripMenuItem
+        '
+        Me.NuevoUsuarioToolStripMenuItem.Name = "NuevoUsuarioToolStripMenuItem"
+        Me.NuevoUsuarioToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.NuevoUsuarioToolStripMenuItem.Text = "Nuevo Socio"
+        '
+        'NueToolStripMenuItem
+        '
+        Me.NueToolStripMenuItem.Name = "NueToolStripMenuItem"
+        Me.NueToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.NueToolStripMenuItem.Text = "Nuevo Viaje"
+        '
+        'NuevaReservaToolStripMenuItem
+        '
+        Me.NuevaReservaToolStripMenuItem.Name = "NuevaReservaToolStripMenuItem"
+        Me.NuevaReservaToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.NuevaReservaToolStripMenuItem.Text = "Nueva Reserva"
+        '
+        'VerToolStripMenuItem
+        '
+        Me.VerToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ResumenToolStripMenuItem})
+        Me.VerToolStripMenuItem.Name = "VerToolStripMenuItem"
+        Me.VerToolStripMenuItem.Size = New System.Drawing.Size(36, 20)
+        Me.VerToolStripMenuItem.Text = "Ver"
+        '
+        'GestionToolStripMenuItem
+        '
+        Me.GestionToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LiquidacionChoferesToolStripMenuItem, Me.LiquidacionCCorrienteToolStripMenuItem})
+        Me.GestionToolStripMenuItem.Name = "GestionToolStripMenuItem"
+        Me.GestionToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.GestionToolStripMenuItem.Text = "Gestion"
+        '
+        'LiquidacionChoferesToolStripMenuItem
+        '
+        Me.LiquidacionChoferesToolStripMenuItem.Name = "LiquidacionChoferesToolStripMenuItem"
+        Me.LiquidacionChoferesToolStripMenuItem.Size = New System.Drawing.Size(199, 22)
+        Me.LiquidacionChoferesToolStripMenuItem.Text = "Liquidacion Choferes"
+        '
+        'LiquidacionCCorrienteToolStripMenuItem
+        '
+        Me.LiquidacionCCorrienteToolStripMenuItem.Name = "LiquidacionCCorrienteToolStripMenuItem"
+        Me.LiquidacionCCorrienteToolStripMenuItem.Size = New System.Drawing.Size(199, 22)
+        Me.LiquidacionCCorrienteToolStripMenuItem.Text = "Liquidacion C.Corriente"
+        '
+        'ClientesToolStripMenuItem
+        '
+        Me.ClientesToolStripMenuItem.Name = "ClientesToolStripMenuItem"
+        Me.ClientesToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.ClientesToolStripMenuItem.Text = "Clientes"
+        '
+        'AyudaToolStripMenuItem
+        '
+        Me.AyudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GuiaToolStripMenuItem, Me.ContactosToolStripMenuItem})
+        Me.AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
+        Me.AyudaToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.AyudaToolStripMenuItem.Text = "Ayuda"
+        '
+        'GuiaToolStripMenuItem
+        '
+        Me.GuiaToolStripMenuItem.Name = "GuiaToolStripMenuItem"
+        Me.GuiaToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.GuiaToolStripMenuItem.Text = "Guia"
+        '
+        'ContactosToolStripMenuItem
+        '
+        Me.ContactosToolStripMenuItem.Name = "ContactosToolStripMenuItem"
+        Me.ContactosToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ContactosToolStripMenuItem.Text = "Contactos"
+        '
+        'ConfiguracionToolStripMenuItem
+        '
+        Me.ConfiguracionToolStripMenuItem.Name = "ConfiguracionToolStripMenuItem"
+        Me.ConfiguracionToolStripMenuItem.Size = New System.Drawing.Size(95, 20)
+        Me.ConfiguracionToolStripMenuItem.Text = "Configuracion"
+        '
+        'ResumenToolStripMenuItem
+        '
+        Me.ResumenToolStripMenuItem.Name = "ResumenToolStripMenuItem"
+        Me.ResumenToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ResumenToolStripMenuItem.Text = "Resumen"
         '
         'Home
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1026, 544)
+        Me.ClientSize = New System.Drawing.Size(1005, 431)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Home"
         Me.Text = "Home"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NuevoUsuarioToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NueToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NuevaReservaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents VerToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ResumenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GestionToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LiquidacionChoferesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LiquidacionCCorrienteToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClientesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AyudaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GuiaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ContactosToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ConfiguracionToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     'Friend WithEvents PrimeraBdDataSet As ProyectoAutoloboOriginal.primeraBdDataSet
     'Friend WithEvents VehiculoTableAdapter As ProyectoAutoloboOriginal.primeraBdDataSetTableAdapters.vehiculoTableAdapter
     ' Friend WithEvents TableAdapterManager As ProyectoAutoloboOriginal.primeraBdDataSetTableAdapters.TableAdapterManager
