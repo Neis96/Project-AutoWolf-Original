2bc351f0a006ad2136869a07000bcc05f0075c77
marran <marrantoti@gmail.com>
2018-09-21 00:03:51 -0300

    bottonsocioshome
    modificaciones de menu

--- ProyectoAutoloboOriginal/ProyectoAutoloboOriginal/Inicio.vb
+++ ProyectoAutoloboOriginal/ProyectoAutoloboOriginal/Inicio.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Inicio
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        bottonhomesocios.Show()
+
+    End Sub
 End Class
