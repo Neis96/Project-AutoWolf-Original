ae0f975798374ae563e7e21dce58a668f09e9320
marran <marrantoti@gmail.com>
2018-09-20 23:19:44 -0300

    Revert "Merge remote-tracking branch 'origin/Develop' into Develop"
    This reverts commit 65f32d6d97f4da4c7fadd72d1448f0a893376740.

--- ProyectoAutoloboOriginal/ProyectoAutoloboOriginal/Module1.vb
+++ ProyectoAutoloboOriginal/ProyectoAutoloboOriginal/Module1.vb
@@ -1,9 +1,4 @@
 ﻿Module Module1
-    Public admin As String = "admin"
-    Public passAdmin As String = "admin"
-    Public user1 As String = "user1"
-    Public passUser1 As String = "1234"
-    Public user2 As String = "user2"
-    Public passUser2 As String = "1234"
+
 
 End Module
