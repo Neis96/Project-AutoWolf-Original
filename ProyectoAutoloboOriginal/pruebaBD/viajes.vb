75f4a19bda04f624adb282b5ce7cd96c685c0b9e
Thomas <thomasjeffery3222@hotmail.com>
2018-10-03 14:31:11 -0300

    Creacion de viajes armada
    Se crea la primera version de facturacion. Aun no se prueba si funciona o no

--- ProyectoAutoloboOriginal/pruebaBD/viajes.vb
+++ ProyectoAutoloboOriginal/pruebaBD/viajes.vb
@@ -21,11 +21,13 @@
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         'boton guardar
-        Dim reserva As String
+        Dim reserva, estado As String
         If CheckBoxReserva.Checked Then
             reserva = "si"
+            estado = "no"
         Else
             reserva = "no"
+            estado = "no"
         End If
         Me.ViajesBindingSource.Current("IdChofer") = Val(ComboBoxChofer.SelectedItem)
         Me.ViajesBindingSource.Current("idSocios") = Val(ComboBoxSocios.SelectedItem)
@@ -33,10 +35,15 @@
         Me.ViajesBindingSource.Current("fecha_origen") = Fecha_destinoDateTimePicker.Value
         Me.ViajesBindingSource.Current("fecha_destino") = Fecha_origenDateTimePicker.Value
         Me.ViajesBindingSource.Current("total") = Val(TotalTextBox.Text)
+        Me.ViajesBindingSource.Current("lugar_origen") = Lugar_origenTextBox.Text
+        Me.ViajesBindingSource.Current("lugar_destino") = Lugar_destinoTextBox.Text
+        Me.ViajesBindingSource.Current("Reserva") = reserva
+        Me.ViajesBindingSource.Current("Finalizado") = estado
+
         Me.ViajesBindingSource.EndEdit()
-        '    Me.TableAdapterManager.UpdateAll(Me.FarmaciaDataSet)
-        '  Me.StockTableAdapter.Fill(Me.FarmaciaDataSet.stock) 'actualizo en este formulario
-        '   Form1.StockTableAdapter.Fill(Form1.FarmaciaDataSet.stock) 'actualizo en formulario principal
-        '   Me.StockBindingSource.AddNew() 'preparo para seguir cargando
+        Me.TableAdapterManager.UpdateAll(Me.Database1DataSet)
+        Me.ViajesTableAdapter.Fill(Me.Database1DataSet.Viajes) 'actualizo en este formulario
+        'Inicio.StockTableAdapter.Fill(Inicio.Database1DataSet.Viajes) 'actualizo en formulario principal
+        Me.ViajesBindingSource.AddNew() 'preparo para seguir cargando
     End Sub
 End Class
